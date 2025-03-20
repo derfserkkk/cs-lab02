4c40ea9b3579ba0b779f0a29cbdb6eecc123731c
Svetlana Rybakova <RybakovaSA@mpei.ru>
2025-03-20 23:33:07 +0300

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Светлана Рыбакова on 20.03.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
