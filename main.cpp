f1ccd5db0ce0f2f0210a0f68a5f8d2d86c6208d8
Svetlana Rybakova <RybakovaSA@mpei.ru>
2025-03-21 00:09:42 +0300

    code: добавлен вывод суммы чисел

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-//
-//  main.cpp
-//  project
-//
-//  Created by Светлана Рыбакова on 20.03.2025.
-//
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    cout << "Sum of A and B: " << a + b << endl;  // Вывод суммы
     return 0;
 }
+
+
