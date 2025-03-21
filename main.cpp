d97c1f325c3977002745c3e83a2297e3e2b44ea6
Bob <bob@example.com>
2025-03-21 12:34:26 +0300

    feat: добавлено вычисление деления чисел

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
+    int a = 5, b = 3;
+    cout << "Произведение чисел: " << a * b << endl;
     cout << "Enter A and B: ";
-    int a, b;
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n';  
+    cout << "Результат деления: " << a / b << endl;
+
     return 0;
 }
